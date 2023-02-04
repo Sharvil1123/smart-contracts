148d5e24a3165f124e898ab0492d2a988e36cdff
sharvil <sharvilbhalke@gmail.com>
2023-02-04 22:57:32 +0530

    .

--- 75_Bank.sol
+++ 75_Bank.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Bank{
-    mapping(address => uint256) public balances;
+    mapping(address  => uint256) public  balances;
     mapping (address => bool) public isStaked;
 
     function deposit() public payable {
